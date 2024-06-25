aaaf2e93d761a4bd2c61eb472a2dd3a0988aafda
João Paulo Ferreira da Silva <jpferreira293@gmail.com>
2024-06-25 17:20:52 -0300

    Segundo commit.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+    const now = new Date();
+    const data = {
+        title: "Express",
+        name: "Jotape",
+        date: now.toLocaleDateString("pt-BR", options),
+        time: now.toLocaleTimeString("pt-BR"),
+        // get.date,getmount.getfullyear
+    };
+    res.render("index", data);
 });
 
 module.exports = router;
